01f704c4a1d2d1e2247960a71eb286c9f0a4a9f0
xxy20020221 <20351008@buaa.edu.cn>
2023-11-18 20:21:34 +0800

    add follow change user

--- ScholarShare/Follow/models.py
+++ ScholarShare/Follow/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from decimal import *  
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.utils import timezone
 # Create your models here.
+from Essay.models import Author
+from UserManage.models import User
+
+class Follow(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='follower')
+    author = models.ForeignKey(Author,on_delete=models.CASCADE,related_name='followee')
