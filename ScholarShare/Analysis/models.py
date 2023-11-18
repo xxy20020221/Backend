0d951d429a2905ca8d7d11a9696b184577f033da
Xury <521028089@qq.com>
2023-11-18 20:20:06 +0800

    analysis init

--- ScholarShare/Analysis/models.py
+++ ScholarShare/Analysis/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from ScholarShare.Essay.models import Work
+from ScholarShare.UserManage.models import User
+
+
 # Create your models here.
+class Analysis(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='analysis')
+    works = models.ManyToManyField(Work, related_name='works', through='Collection', blank=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    file_url = models.CharField(default='', max_length=128, verbose_name='路径')
+    file = models.FileField(default='', upload_to='analysis/', verbose_name='解析')
+
+    class Meta:
+        db_table = 'Analysis'
